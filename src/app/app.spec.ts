f039ba370cfbb77eda69ef039f9ac9f41891515f
David Jaramillo <davidjaramillo915@gmail.com>
2025-09-19 19:07:07 -0500

    se ajusta el app.spec

--- src/app/app.spec.ts
+++ src/app/app.spec.ts
@@ -1,11 +1,14 @@
 // src/app/app.spec.ts
 import { TestBed } from '@angular/core/testing';
-import { App } from './app'; // 
+import { provideRouter } from '@angular/router';
+import { App } from './app';
+import { routes } from './app.routes'; 
 
 describe('App', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [App], 
+      imports: [App],                 // App standalone
+      providers: [provideRouter(routes)], 
     }).compileComponents();
   });
 
