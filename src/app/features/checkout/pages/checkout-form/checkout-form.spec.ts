badfb5e848b1cf8751659f79e8a2b5f83b560c7a
David Jaramillo <davidjaramillo915@gmail.com>
2025-09-20 14:58:18 -0500

    se arregla el spec del checkout

--- src/app/features/checkout/pages/checkout-form/checkout-form.spec.ts
+++ src/app/features/checkout/pages/checkout-form/checkout-form.spec.ts
@@ -1,13 +1,17 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing'; // 👈 importa el módulo de testing de router
 import { CheckoutFormComponent } from './checkout-form.component';
 
-describe('CheckoutFormComponent', () => {  
+describe('CheckoutFormComponent', () => {
   let component: CheckoutFormComponent;
   let fixture: ComponentFixture<CheckoutFormComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [CheckoutFormComponent]
+      imports: [
+        RouterTestingModule,      // 👈 provee ActivatedRoute, RouterLink, etc.
+        CheckoutFormComponent     // 👈 si es standalone, va aquí en imports
+      ]
     }).compileComponents();
 
     fixture = TestBed.createComponent(CheckoutFormComponent);
